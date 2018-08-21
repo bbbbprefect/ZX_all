0576b664427345cd13ff048def30769c31320d08
bbbbprefect <30856827+bbbbprefect@users.noreply.github.com>
2018-08-21 17:20:21 +0800

    Add files via upload

---
+++ ZX_all/ZX_all/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZX_all
+//
+//  Created by 赵祥 on 2018/8/20.
+//  Copyright © 2018年 赵祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZX_all/ZX_all/ViewController.m
@@ -0,0 +1,79 @@
+//
+//  ViewController.m
+//  ZX_all
+//
+//  Created by 赵祥 on 2018/8/20.
+//  Copyright © 2018年 赵祥. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "editorViewController.h"
+
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property(nonatomic,strong)UITableView *tableView;
+
+@property(nonatomic)NSMutableArray *dataArray;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.dataArray = [[NSMutableArray alloc]initWithObjects:@"富文本编辑器", nil];
+    
+    self.tableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self.view addSubview:self.tableView];
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 1;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    return 80;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
+    }
+    //cell选中效果
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    
+    cell.textLabel.text = [_dataArray objectAtIndex:indexPath.row];
+    NSLog(@"%ld",(long)indexPath.row);
+    cell.textLabel.textColor = [UIColor blueColor];
+    return cell;
+}
+
+//选中时 调用的方法
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"选中didSelectRowAtIndexPath row = %ld", indexPath.row);
+    //富文本跳转
+    if(indexPath.row == 0)
+    {
+        editorViewController *eVC = [[editorViewController alloc]init];
+        [self.navigationController pushViewController:eVC animated:YES];
+    }
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
